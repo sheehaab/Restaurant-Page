61f5b4c823ff775e69505c9e3465d8c79e4b08e4
sheehaab <shehabahmed963@gmail.com>
2020-12-19 10:59:17 +0200

    conver the code to the javaScript dom

--- src/about.js
+++ src/about.js
@@ -29,6 +29,26 @@ let page = ()=>{
 	liOne.textContent = 'home';
 	liTwo.textContent = 'menu';
 	liThree.textContent = 'about';
+
+	//create the about 
+	const about = document.createElement('div');
+	about.setAttribute('id','about');
+	content.appendChild(about);
+
+	//create text div
+	const text = document.createElement('div');
+	text.classList.add('text');
+	about.appendChild(text);
+
+
+	//make the h2 and p
+	const hTwo = document.createElement('h2');
+	hTwo.textContent = 'About';
+	const p = document.createElement('p');
+	p.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget nunc dictum, blandit dui nec, molestie eros. Morbi semper quam vitae dolor tristique volutpat. Phasellus luctus n.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget nunc dictum, blandit dui nec, molestie eros. Morbi semper quam vitae dolor tristique volutpat. Phasellus luctus n.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget nunc dictum, blandit dui nec, molestie eros. Morbi semper quam vitae dolor tristique volutpat. Phasellus luctus n.'
+	text.appendChild(hTwo);
+	text.appendChild(p);
+
 }
 
 export {page};
